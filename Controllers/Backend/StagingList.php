a8df5b12366bdd0652c3c5ceb4a144e3d7a15ac9
Daniel Wolf <daniel.wolf@8mylez.com>
2018-06-02 14:23:19 +0200

    Change save method.

--- Controllers/Backend/StagingList.php
+++ Controllers/Backend/StagingList.php
@@ -1,7 +1,29 @@
 <?php
 
+use EmzStagingEnvironmentDeluxe\Models\Staging\Staging;
+
 class Shopware_Controllers_Backend_StagingList extends Shopware_Controllers_Backend_Application
 {
     protected $model = 'EmzStagingEnvironmentDeluxe\Models\Staging\Staging';
     protected $alias = 'staging';
+
+    public function save($data)
+    {
+        $staging = new Staging();
+
+        $staging->setName($data['name']);
+        $staging->setCreatedOn(date("Y-m-d"));
+        $staging->setDbHost($data['dbHost']);
+        $staging->setDbPort($data['dbPort']);
+        $staging->setDbName($data['dbName']);
+        $staging->setDbUser($data['dbUser']);
+        $staging->setDbPassword($data['dbPassword']);
+        $staging->setExcludedFolders($data['excludedFolders']);
+        $staging->setStagingConfig('lol');
+
+        $em = $this->get('models');
+
+        $em->persist($staging);
+        $em->flush();
+    }
 }
--- Resources/Views/backend/staging_list/model/main.js
+++ Resources/Views/backend/staging_list/model/main.js
@@ -13,6 +13,12 @@ Ext.define('Shopware.apps.StagingList.model.Main', {
     fields: [
         { name : 'id', type: 'int', useNull: true },
         { name : 'name', type: 'string' },
-        { name : 'createdOn', type: 'date' },
+        { name : 'createdOn', type: 'date', useNull: false },
+        { name : 'dbHost', type: 'string' },
+        { name : 'dbPort', type: 'string' },
+        { name : 'dbName', type: 'string' },
+        { name : 'dbUser', type: 'string' },
+        { name : 'dbPassword', type: 'string' },
+        { name : 'excludedFolders', type: 'string', useNull: false },
     ]
 });

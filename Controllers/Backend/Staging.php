c85ed06d82074883adccab6d0ceb927643f79fb2
Daniel Wolf <daniel.wolf@8mylez.com>
2018-06-25 04:38:28 +0200

    clean old code.

--- Controllers/Backend/Staging.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Shopware_Controllers_Backend_Staging extends Shopware_Controllers_Backend_Application
-{
-    protected $model = 'EmzStagingEnvironmentDeluxe\Models\Staging\Staging';
-    protected $alias = 'staging';
-}

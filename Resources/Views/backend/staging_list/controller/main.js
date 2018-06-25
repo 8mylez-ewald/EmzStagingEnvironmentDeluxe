d75a73a4d2eafb5c3ed79fa3d64b70100118b968
Daniel Wolf <daniel.wolf@8mylez.com>
2018-06-25 04:24:02 +0200

    Remove unused code.

--- Resources/Views/backend/staging_list/controller/main.js
+++ Resources/Views/backend/staging_list/controller/main.js
@@ -6,20 +6,6 @@ Ext.define('Shopware.apps.StagingList.controller.Main', {
         var me = this;
         me.mainWindow = me.getView('list.Window').create({ }).show();
 
-        me.control({
-            'emz-staging-listing-grid': {
-                'main-add-item': me.onAddItem,
-            },
-        });
-
         me.callParent(arguments);
     },
-
-    onAddItem: function() {
-        var me = this;
-
-        Shopware.app.Application.addSubApplication({
-            name: 'Shopware.apps.Staging'
-        });
-    }
 });
--- Resources/Views/backend/staging_list/view/detail/window.js
+++ Resources/Views/backend/staging_list/view/detail/window.js
@@ -1,9 +1,17 @@
-
 Ext.define('Shopware.apps.StagingList.view.detail.Window', {
-    extend: 'Shopware.window.Detail',
-    alias: 'widget.emz-staging-detail-window',
-
-    title : '{s name=title}StagingList details{/s}',
+    extend : 'Shopware.window.Detail',
+    alias : 'widget.emz-staging-detail-window',
+    title : '{s name=title}Staging Detailansicht{/s}',
     height: 420,
-    width: 900
+    width: 900,
+
+    /**
+     * Initialize the component
+     * @return void
+     */
+    initComponent : function () {
+        var me = this;
+
+        me.callParent(arguments);
+    },
 });

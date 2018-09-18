b4966160bc6cd14b1bffdd34c261621dc5f59aa0
ewald.gering <ewald.gering@8mylez.com>
2018-09-18 15:40:28 +0200

    fix save function for staging

--- Controllers/Backend/StagingList.php
+++ Controllers/Backend/StagingList.php
@@ -6,17 +6,4 @@ class Shopware_Controllers_Backend_StagingList extends Shopware_Controllers_Back
 {
     protected $model = 'EmzStagingEnvironmentDeluxe\Models\Staging\Staging';
     protected $alias = 'staging';
-
-    public function save($data)
-    {
-        $staging = new Staging();
-
-        $staging->fromArray($data);
-        $staging->setCreatedOn(date("Y-m-d"));
-
-        $em = $this->get('models');
-
-        $em->persist($staging);
-        $em->flush();
-    }
 }
